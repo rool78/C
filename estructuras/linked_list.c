1ca6f7c2b4a1a8f95ab33fbd0118d24f9d944173
rooL78 <rmaza@oesia.com>
2020-05-19 08:34:17 +0200

    linked list

--- estructuras/linked_list.c
+++ estructuras/linked_list.c
@@ -28,14 +28,11 @@ and an int, inserts a new node on the front of the list. */
 void push(struct Node** head_ref, int new_data) 
 { 
 	/* 1. allocate node */
-	struct Node* new_node = (struct Node*) malloc(sizeof(struct Node)); 
-
+	struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
 	/* 2. put in the data */
 	new_node->value = new_data; 
-
 	/* 3. Make next of new node as head */
 	new_node->next = (*head_ref); 
-
 	/* 4. move the head to point to the new node */
 	(*head_ref) = new_node; 
 } 
@@ -43,13 +40,62 @@ void push(struct Node** head_ref, int new_data)
 /*TODO Insert after
  We are given pointer to a node, and the new node is inserted after the given node.*/
 void insertAfter(struct Node* prev_node, int new_data){
+    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+
+    new_node->value = new_node;
+    new_node->next = prev_node->next;
+
+    prev_node->next = new_node;
 
 }
 
 /*TODO Append
 Add a node at the end*/
 void append(struct Node** head_ref, int new_data){
+    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+
+    new_node->value = new_data;
+    new_node->next = NULL;
     
+    struct Node* n = (*head_ref);
+    
+    while(n->next != NULL){
+        n = n->next;
+    }
+
+    if(n->next == NULL){
+        n->next = new_node;
+    }
+}
+
+void delete(struct Node** head_ref, int key){
+    struct Node* n = (*head_ref);
+
+    if((*head_ref)->value == key){
+
+    }
+
+    if(n->value == key){
+        n->value = NULL;
+        return;
+    }
+
+    while(n->next != NULL){
+        if(n->next->value == key){
+            //delete
+            struct Node* tmp = n->next;
+            if(tmp->next != NULL){
+                n->next = tmp->next;
+            }else{
+                n->next = NULL;
+            }
+            free(tmp);
+            return;
+        }else{
+            n = n->next;
+        }
+        
+    }
 }
 
 // Program to create a simple linked 
@@ -61,6 +107,19 @@ int main(){
 
     push(&head, 78);
     push(&head,33);
+    printList(head);
+
+    printf("\nProbando Append:");
+
+    append(&head, 57);
+    append(&head, 23);
+    printList(head);
+
+    printf("\nProbando Delete:");
+
+
+    delete(&head, 57);
+
     printList(head);
 
     return 0;
